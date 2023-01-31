ef7c1f3f669a4fd36f804a9f20a4ee93e5652a7c
Ethan Guo <eguo@isoton.com>
2023-01-31 17:32:00 +1030

    add: gradient color and icons to headerbar

--- ethan_personal_page/src/App.jsx
+++ ethan_personal_page/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 
-import { HeaderBar } from "../src/components/HeaderBar";
+import HeaderBar from "../src/components/HeaderBar";
 
 function App() {
   return (
-    <div className='container mx-auto p-4'>
-      <HeaderBar></HeaderBar>
+    <div className='mx-auto p-4 bg-gradient-to-b from-black to-white'>
+      <HeaderBar />
     </div>
   );
 }
--- ethan_personal_page/src/components/HeaderBar.jsx
+++ ethan_personal_page/src/components/HeaderBar.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import Github from "../assets/Github.svg";
+import Instagram from "../assets/Instagram.svg";
+import LinkedIn from "../assets/LinkedIn.svg";
 import Profile from "../assets/ProfilePhoto.jpg";
 
 function HeaderBar() {
   return (
     <header className='flex justify-between items-center h-20'>
-      <img src={Profile} alt='' className='w-8 h-8'/>
+      <img src={Profile} alt='' className='w-12 h-12 rounded-full' href='' />
+
+      <div className='container'>
+        <button className='ml-6 text-white font-bold hover:text-gray-400 '>
+          {"Education"}
+        </button>
+
+        <button className='ml-6 text-white font-bold hover:text-gray-400 '>
+          {"Projects"}
+        </button>
+      </div>
+
+      <nav className='flex'>
+        <div className='flex flex-row items-center'>
+          <a href='https://github.com/EasonGuo666' className=''>
+            <img
+              src={Github}
+              className='w-7 h-7 mx-4 my=2 hover:bg-gray-300 hover:rounded-full'
+              alt=''
+            />
+          </a>
+
+          <a href='https://www.instagram.com/guo_yixuan666/'>
+            <img
+              src={Instagram}
+              className='w-7 h-7 mx-4 my=2 hover:bg-gray-300 hover:rounded-full'
+              alt=''
+            />
+          </a>
+
+          <a href='https://www.linkedin.com/in/yixuan-guo-35372b1a9/'>
+            <img
+              src={LinkedIn}
+              className='w-7 h-7 mx-4 my=2 hover:fill-slate-700'
+              alt=''
+            />
+          </a>
+        </div>
+      </nav>
     </header>
   );
 }
