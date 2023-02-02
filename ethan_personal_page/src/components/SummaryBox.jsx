d2fda7cc69a8d030d14755526e9e051a18c2d865
Yixuan <easonguo100@outlook.com>
2023-02-02 15:21:21 +1030

    add: a greeting cover

--- ethan_personal_page/src/components/SummaryBox.jsx
+++ ethan_personal_page/src/components/SummaryBox.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function SummaryBox() {
   return (
+    //
     <div className="w-full h-full bg-[url('./src/assets/Background.jpg')]">
-      <div className='w-full'>
-        summary box
+      <div className='w-full py-80 text-center font-sans text-5xl font-bold text-white'>
+        Hi, I'm Ethan!<br/>
+        Welcome to my space
       </div>
     </div>
   )
