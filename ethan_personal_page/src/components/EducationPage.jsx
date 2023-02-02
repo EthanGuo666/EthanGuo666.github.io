0cd08958332e99dbba65f24bf5cf6fcdf6e1705c
Yixuan <easonguo100@outlook.com>
2023-02-02 17:16:18 +1030

    add: the education page component

---
+++ ethan_personal_page/src/components/EducationPage.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function EducationPage() {
+  return (
+    <div className='flex w-full h-full bg-zinc-300'>
+      <div className='w-full m-auto text-center '>
+        <span className='font-sans text-3xl font-bold text-stone-500'>
+          Education
+        </span>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      </div>
+    </div>
+  );
+}
+
+export default EducationPage;
