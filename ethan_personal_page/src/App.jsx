34d73cf4e3eded3dded4977bcfe22e633f87500e
Yixuan <easonguo100@outlook.com>
2023-02-02 11:33:45 +1030

    fix: summary box size issue

--- ethan_personal_page/src/App.jsx
+++ ethan_personal_page/src/App.jsx
@@ -5,83 +5,15 @@ import SummaryBox from "./components/SummaryBox";
 
 function App() {
   return (
-    <div className="bg-[url('./src/assets/Background.jpg')]">
+    <div>
       <div className='mx-auto p-4 bg-gradient-to-b from-black to-transparent via-transparent fixed w-full'>
         <HeaderBar />
       </div>
-      <div className='container'>
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
-        <SummaryBox />
+      <div className="w-full h-screen">
+        <SummaryBox />
+      </div>
+      <div className="w-full h-screen">
+        Hello
       </div>
     </div>
   );
--- ethan_personal_page/src/components/SummaryBox.jsx
+++ ethan_personal_page/src/components/SummaryBox.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function SummaryBox() {
   return (
-    <div>SummaryBox</div>
+    <div className="w-full h-full bg-[url('./src/assets/Background.jpg')]">
+      <div className='w-full'>
+        summary box
+      </div>
+    </div>
   )
 }
 
