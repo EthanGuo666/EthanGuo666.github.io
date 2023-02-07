b771f784899ac06ad16ea046f587681d54f9af9c
Yixuan <easonguo100@outlook.com>
2023-02-07 16:45:32 +1030

    fix: bring the headerbar to the top layer

--- ethan_personal_page/src/App.jsx
+++ ethan_personal_page/src/App.jsx
@@ -8,7 +8,7 @@ import ProjectPage from "./components/ProjectPage";
 function App() {
   return (
     <div>
-      <div className='fixed w-full mx-auto p-4 bg-gradient-to-b from-black to-transparent'>
+      <div className='fixed w-full mx-auto p-4 z-50 bg-gradient-to-b from-black to-transparent'>
         <HeaderBar />
       </div>
       <div className="w-full h-screen">
