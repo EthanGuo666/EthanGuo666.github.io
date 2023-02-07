4872a9d4b3853169144e5b0357feaac53e7c8014
Yixuan <easonguo100@outlook.com>
2023-02-07 17:19:43 +1030

    fix: background image fit screen

--- ethan_personal_page/src/components/SummaryBox.jsx
+++ ethan_personal_page/src/components/SummaryBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SummaryBox() {
   return (
     //
-    <div className="flex w-full h-full bg-[url('./src/assets/Background.jpg')]" id='page-top'>
+    <div className="flex w-full h-full bg-[url('./src/assets/Background.jpg')] bg-cover" id='page-top'>
       <div className='w-full m-auto text-center font-sans text-5xl font-bold text-white'>
         <span>Hi, I'm Ethan!<br/></span>
         <span>Welcome to my space...</span>
