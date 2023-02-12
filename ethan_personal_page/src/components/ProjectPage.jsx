362dd4d136cd1db48954868d78d2ea75b5484f70
Yixuan <easonguo100@outlook.com>
2023-02-12 22:56:28 +1030

    add: tabs to allow switching between projects

--- ethan_personal_page/src/components/ProjectPage.jsx
+++ ethan_personal_page/src/components/ProjectPage.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useState } from "react";
 
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -7,7 +11,40 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import "../styles/Canvas.css";
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
 function ProjectPage() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className='Canvas text-center'>
       <br />
@@ -18,6 +55,27 @@ function ProjectPage() {
       <br />
       <br />
       <br />
+
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label='basic tabs example'
+        >
+          <Tab label='Item One' {...a11yProps(0)} />
+          <Tab label='Item Two' {...a11yProps(1)} />
+          <Tab label='Item Three' {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
 
       <div className='m-5'>
         <div className='grid gap-4 grid-cols-2 grid-rows-2'>
