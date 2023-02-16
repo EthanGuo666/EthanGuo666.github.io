0f4b921c8afb4d51d8dfa8690e07b0a680b42437
Yixuan <easonguo100@outlook.com>
2023-02-16 10:50:17 +1030

    feat: put cards in the grid
    BREAKING CHANGE: card style update

--- ethan_personal_page/src/components/ProjectPage.jsx
+++ ethan_personal_page/src/components/ProjectPage.jsx
@@ -48,29 +48,106 @@ function ProjectPage() {
     <div className='Canvas text-center'>
       <br />
       <br />
+      <br />
+      <br />
+      <br />
       <span className='font-sans text-3xl font-bold text-stone-500'>
         Projects
       </span>
       <br />
       <br />
-      <br />
 
       <div className='flex flex-wrap justify-center'>
-        <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-10'>
-          <div className='bg-gray-200 h-80'>Object 1</div>
+        <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-14'>
+          {/* <div className='bg-gray-200 h-80 rounded-3xl hover:shadow-2xl'>
+            Object 1
+          </div> */}
+          <Card className='hover:shadow-2xl rounded-2xl'>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                image='/src/assets/picturePlaceHolder.png'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                  Avartus
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Description project 1
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </div>
-        <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-10'>
-          <div className='bg-gray-200 h-80'>Object 2</div>
+
+        <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-14'>
+          {/* <div className='bg-gray-200 h-80 rounded-3xl hover:shadow-2xl'>
+            Object 2
+          </div> */}
+          <Card className='hover:shadow-2xl'>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                image='/src/assets/picturePlaceHolder.png'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                  Project 2
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Description project 2
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </div>
-        <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-10'>
-          <div className='bg-gray-200 h-80'>Object 3</div>
+
+        <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-14'>
+          {/* <div className='bg-gray-200 h-80 rounded-3xl hover:shadow-2xl'>
+            Object 3
+          </div> */}
+          <Card className='hover:shadow-2xl'>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                image='/src/assets/picturePlaceHolder.png'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                  Project 3
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Description project 3
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </div>
-        <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-10'>
-          <div className='bg-gray-200 h-80'>Object 4</div>
+
+        <div className='w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-14'>
+          {/* <div className='bg-gray-200 h-80 rounded-3xl hover:shadow-2xl'>
+            Object 4
+          </div> */}
+          <Card className='hover:shadow-2xl'>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                image='/src/assets/picturePlaceHolder.png'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                  Project 4
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Description project 4
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </div>
       </div>
 
-      <div className='m-5'>
+      {/* <div className='m-5'>
         <div className='grid gap-4 grid-cols-2 grid-rows-2'>
           <div className='mx-auto'>
             <Card sx={{ maxWidth: 345 }} className='hover:shadow-2xl'>
@@ -156,7 +233,9 @@ function ProjectPage() {
             </Card>
           </div>
         </div>
-      </div>
+      </div> */}
+
+
     </div>
   );
 }
