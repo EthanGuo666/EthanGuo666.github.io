1c24998c43c02fd1ad3028d452529fee80ce5504
Yixuan <easonguo100@outlook.com>
2023-02-28 11:47:51 +1030

    feat: typewriter effect on text

--- ethan_personal_page/src/components/SummaryBox.jsx
+++ ethan_personal_page/src/components/SummaryBox.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function SummaryBox() {
+  const [text, setText] = useState('');
+  const [isTyping, setIsTyping] = useState(true);
+
+  useEffect(() => {
+    if (isTyping) {
+      const intervalId = setInterval(() => {
+        setText((prevText) =>
+          prevText.length === message.length ? prevText : message.slice(0, prevText.length + 1)
+        );
+      }, 200);
+      return () => clearInterval(intervalId);
+    }
+  });
+
+  const message = "Hi, I'm Ethan! \n\nWelcome to my space...";
+
   return (
     //
     <div className="flex w-full h-full bg-[url('./src/assets/Background.jpg')] bg-cover" id='page-top'>
       <div className='w-full m-auto text-center font-sans text-5xl font-bold text-white'>
-        <span>Hi, I'm Ethan!<br/></span>
-        <span>Welcome to my space...</span>
+        {/* <span>Hi, I'm Ethan!<br/></span> */}
+        {text}
+        <span className="text-yellow-300 animate-spin">|</span>
       </div>
     </div>
   )
