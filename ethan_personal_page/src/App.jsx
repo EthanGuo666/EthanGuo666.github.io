db8a85eb7c6fedb93ab061b120d694570b82b007
Yixuan <easonguo100@outlook.com>
2023-03-08 14:39:04 +1030

    feat: remove headerbar from project detail page

--- ethan_personal_page/src/App.jsx
+++ ethan_personal_page/src/App.jsx
@@ -1,20 +1,16 @@
 import reactLogo from "./assets/react.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HeaderBar from "./components/HeaderBar";
 import HomePage from "./pages/HomePage";
+import Avartus from "./pages/ProjectDetailPages/Avartus";
 
 function App() {
   return (
     <div>
-      <div className='fixed w-full mx-auto p-4 z-50 bg-gradient-to-b from-black to-transparent'>
-        <HeaderBar />
-      </div>
-
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />}></Route>
-          <Route path='/haha' element={"haha"}></Route>
+          <Route path='/projects/avartus' element={<Avartus />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
