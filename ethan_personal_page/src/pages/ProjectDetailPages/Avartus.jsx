55ef7852eb61225925c616e188f7c495b7ed6859
Yixuan <easonguo100@outlook.com>
2023-03-09 17:26:56 +1030

    feat: add picture for project Avartus

--- ethan_personal_page/src/pages/ProjectDetailPages/Avartus.jsx
+++ ethan_personal_page/src/pages/ProjectDetailPages/Avartus.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../../styles/Canvas.css";
 import ProjectHeader from "./ProjectHeader";
-import CMU from "../../assets/CMU.jpeg";
+import CMU from "../../assets/CMU.jpg";
 
 function Avartus() {
   return (
     <div>
       <ProjectHeader />
+
+      <img src={CMU} className='w-screen h-80 object-cover' />
+      <div className="absolute inset-0 w-full pt-36 text-center">
+        <h1 className='text-white text-4xl font-serif'>Avartus</h1>
+        <p className="text-gray-200 font-extralight italic">A teaching facility remote control <br/> system of CMU IT Dept.</p>
+      </div>
 
       {/* Text area within the p-4 */}
       <div className='Canvas'>
