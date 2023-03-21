89fa5564fb1d37109bc2b7a8bdc0e40cdea5fd06
Yixuan <easonguo100@outlook.com>
2023-03-21 15:23:35 +1030

    feat: add responsive design to the text area
    BREAKING CHANGE: shrink to 3/5 width of the screen on large screens

--- ethan_personal_page/src/pages/ProjectDetailPages/Avartus.jsx
+++ ethan_personal_page/src/pages/ProjectDetailPages/Avartus.jsx
@@ -18,48 +18,49 @@ function Avartus() {
         </p>
       </div>
 
-      {/* Text area within the p-4 */}
-      <div className='Canvas font-serif'>
-        <div className='px-6 pt-6'>
-          <span className='text-4xl'>T</span>
-          he concept of Avartus was first conceived in 2020 to assist faculty
-          members with their daily work at CMU. During the covid lockdown, the
-          campus complied with the SA government protocol that restricted the
-          access to public spaces for people who have symptoms. The university
-          needed a solution to manipulate the teaching facilities in building
-          remotely. This is when Avartus proved to be an indispensable tool.
-        </div>
-
-        {/* todo: image and text responsive to the screen size */}
-        <div className='m-6 items-center text-center'>
-          <div className='flex justify-center items-center'>
-            <img src={PicturePlaceHolder} className='w-1/2 rounded-2xl' />
+      {/* Text area within the w-screen */}
+      <div className='Canvas font-serif flex justify-center text-left'>
+        <div className='w-screen md:w-3/5'>
+          <div className='px-6 pt-6'>
+            <span className='text-4xl'>T</span>
+            he concept of Avartus was first conceived in 2020 to assist faculty
+            members with their daily work at CMU. During the covid lockdown, the
+            campus complied with the SA government protocol that restricted the
+            access to public spaces for people who have symptoms. The university
+            needed a solution to manipulate the teaching facilities in building
+            remotely. This is when Avartus proved to be an indispensable tool.
           </div>
-          <p className='text-xs text-gray-400'>Login Page</p>
-        </div>
 
-        <div className='px-6'>
-          Fortunately, I joined this innovative project with a group of interns
-          from CMU. Before we start working on the project, it already had a
-          functioning backend built in Golang but there is no UI implementation
-          at that point. So our job in this project was to construct its
-          frontend from the scratch. I completed some of its key components and
-          functions like login, a series of settings related to user account,
-          adding/dropping device page and so on.
-        </div>
-
-        <div className='m-6 items-center text-center'>
-          <div className='flex justify-center items-center'>
-            <img src={PicturePlaceHolder} className='w-1/2 rounded-2xl' />
+          <div className='m-6 items-center text-center'>
+            <div className='flex justify-center items-center'>
+              <img src={PicturePlaceHolder} className='w-1/2 rounded-2xl' />
+            </div>
+            <p className='text-xs text-gray-400'>Login Page</p>
           </div>
-          <p className='text-xs text-gray-400'>Account Settings Page</p>
-        </div>
 
-        <div className='px-6'>
-          The management interface of Avartus is built with React Typescript and
-          TailwindCSS. Once loging in with proper username and password, the
-          user will store a JWT token in local from the response for future
-          communications.
+          <div className='px-6'>
+            Fortunately, I joined this innovative project with a group of
+            interns from CMU. Before we start working on the project, it already
+            had a functioning backend built in Golang but there is no UI
+            implementation at that point. So our job in this project was to
+            construct its frontend from the scratch. I completed some of its key
+            components and functions like login, a series of settings related to
+            user account, adding/dropping device page and so on.
+          </div>
+
+          <div className='m-6 items-center text-center'>
+            <div className='flex justify-center items-center'>
+              <img src={PicturePlaceHolder} className='w-1/2 rounded-2xl' />
+            </div>
+            <p className='text-xs text-gray-400'>Account Settings Page</p>
+          </div>
+
+          <div className='px-6'>
+            The management interface of Avartus is built with React Typescript
+            and TailwindCSS. Once loging in with proper username and password,
+            the user will store a JWT token in local from the response for
+            future communications.
+          </div>
         </div>
       </div>
       <PageFooter />
