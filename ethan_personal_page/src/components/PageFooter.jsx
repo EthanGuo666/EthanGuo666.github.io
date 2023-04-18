b1d3fc956b03c0b767d5c11c8a4de66287c1dbda
Yixuan <easonguo100@outlook.com>
2023-04-18 16:43:13 +0930

    feat: update the copyright footer

--- ethan_personal_page/src/components/PageFooter.jsx
+++ ethan_personal_page/src/components/PageFooter.jsx
@@ -7,10 +7,7 @@ function PageFooter() {
       <div className='pt-14'></div>
       <footer className='flex p-4 justify-center items-center'>
         <p className='text-gray-400 text-xs text-center'>
-          &copy;2023 Ethan Guo. All Rights Reserved. Contact:&nbsp;
-          <a href='mailto:easonguo100@outlook.com'>
-            easonguo100@outlook.com
-          </a>
+          Copyright &copy; 2023 Ethan Guo.<br /> All Rights Reserved.
         </p>
       </footer>
     </div>
