cd02d1d3a323f2a68b223fd97b8d6b29f5edff6b
Yixuan <easonguo100@outlook.com>
2023-05-10 10:54:46 +0930

    feat: add project description for S28

--- ethan_personal_page/src/pages/ProjectDetailPages/Section28Cheese.jsx
+++ ethan_personal_page/src/pages/ProjectDetailPages/Section28Cheese.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../../styles/Canvas.css";
 import ProjectPageHeader from "./ProjectPageHeader";
 import CMU from "../../assets/CMU.jpg";
+import Section28CheeseBlurred from "../../assets/Section28CheeseBlurred.jpg";
 import PicturePlaceHolder from "../../assets/PicturePlaceHolder.png";
 import AvartusLoginPage from "../../assets/AvartusLoginPage.jpeg";
 import AvartusSettingsPage from "../../assets/AvartusSettingsPage.jpeg";
@@ -12,10 +13,13 @@ function Section28Cheese() {
     <div>
       <ProjectPageHeader />
 
-      <img src={PicturePlaceHolder} className='w-screen h-80 object-cover' />
+      <img
+        src={Section28CheeseBlurred}
+        className='w-screen h-80 object-cover'
+      />
       <div className='absolute inset-0 w-full pt-32 text-center'>
         <h1 className='text-white text-4xl font-serif'>Section 28 Cheese</h1>
-        <p className='text-gray-200 font-extralight italic'>
+        <p className='text-white font-extralight italic'>
           The digitalization solution for
           <br />a prize-winning cheese manufacturer.
         </p>
@@ -25,8 +29,11 @@ function Section28Cheese() {
       <div className='Canvas font-serif flex justify-center text-left'>
         <div className='w-screen md:w-3/5'>
           <div className='px-6 pt-6'>
-            <span className='text-4xl'>T</span>
-            he
+            <span className='text-2xl'>T</span>he story begins with our client,
+            <span className='text-gray-400 italic'> Section 28</span>, a local
+            artisan cheese maker located in South Australia. Their signature
+            product, Mont Priscilla, is a semi-soft cheese that stands out due
+            to the distinguishtive ash line through its centre.
           </div>
 
           <div className='m-6 items-center text-center'>
@@ -41,8 +48,19 @@ function Section28Cheese() {
 
           <div className='px-6'>
             <p>
-              p1
+              The existing material management system they were using is just
+              putting everything in an excel sheet. As the company made more and
+              more cheese, the excel sheet gets unmanageably large. The owner of
+              s28 does not plan to introduce a fully automated production line
+              in order to retain the artisanal cheese flavor to the maximum.
+              However, they do want a digitalization solution for their material
+              management process to monitor the ripening of each piece of cheese
+              and do order tracking.
             </p>
+          </div>
+
+          <div className='px-6 pt-6'>
+            <p>1</p>
           </div>
 
           <div className='m-6 items-center text-center'>
@@ -56,9 +74,7 @@ function Section28Cheese() {
           </div>
 
           <div className='px-6'>
-            <p>
-              p2
-            </p>
+            <p>p2</p>
           </div>
         </div>
       </div>
