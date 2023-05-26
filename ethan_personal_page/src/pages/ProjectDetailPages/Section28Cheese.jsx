4881eadba3ac633a97bde1f78c713f2d20611d0f
Yixuan <easonguo100@outlook.com>
2023-05-27 00:30:23 +0930

    feat: finish the intro of section28cheese proj

--- ethan_personal_page/src/pages/ProjectDetailPages/Section28Cheese.jsx
+++ ethan_personal_page/src/pages/ProjectDetailPages/Section28Cheese.jsx
@@ -1,12 +1,10 @@
 import React from "react";
+import PicturePlaceHolder from "../../assets/PicturePlaceHolder.png";
+import Section28CheeseBlurred from "../../assets/Section28CheeseBlurred.jpg";
+import Section28CheeseLogin from "../../assets/Section28CheeseLogin.jpg";
+import PageFooter from "../../components/PageFooter";
 import "../../styles/Canvas.css";
 import ProjectPageHeader from "./ProjectPageHeader";
-import CMU from "../../assets/CMU.jpg";
-import Section28CheeseBlurred from "../../assets/Section28CheeseBlurred.jpg";
-import PicturePlaceHolder from "../../assets/PicturePlaceHolder.png";
-import AvartusLoginPage from "../../assets/AvartusLoginPage.jpeg";
-import AvartusSettingsPage from "../../assets/AvartusSettingsPage.jpeg";
-import PageFooter from "../../components/PageFooter";
 
 function Section28Cheese() {
   return (
@@ -32,15 +30,32 @@ function Section28Cheese() {
             <span className='text-2xl'>T</span>he story begins with our client,
             <span className='text-gray-400 italic'> Section 28</span>, a local
             artisan cheese maker located in South Australia. Their signature
-            product, Mont Priscilla, is a semi-soft cheese that stands out due
-            to the distinguishtive ash line through its centre.
+            product, Mont Priscilla, has gained some local fame and has even won
+            awards in cheese competitions. To produce a high-quality cheese,
+            strict control is required at every step, from the procurement of
+            raw materials to the cleaning and fermentation of the cheese. This
+            is where this digitalization project comes into the picture.
+          </div>
+
+          <div className='px-6 pt-6'>
+            <p>
+              It is necessary to note in advance that the owner of s28 does not
+              plan to introduce a fully automated production line in order to
+              retain the artisanal cheese flavor to the maximum. The existing
+              material management system they were using is just putting
+              everything in an excel sheet. As the company made more and more
+              cheese, the excel sheet becomes large and unmanageable. Therefore,
+              they do want a digitalized material management program to record
+              the ripening condition of each piece of cheese and do order
+              tracking.
+            </p>
           </div>
 
           <div className='m-6 items-center text-center'>
             <div className='flex justify-center items-center'>
               <img
-                src={PicturePlaceHolder}
-                className='w-3/5 rounded-md shadow-xl'
+                src={Section28CheeseLogin}
+                className='w-4/5 rounded-md shadow-2xl'
               />
             </div>
             <p className='text-xs text-gray-400'>Login Page</p>
@@ -48,33 +63,11 @@ function Section28Cheese() {
 
           <div className='px-6'>
             <p>
-              The existing material management system they were using is just
-              putting everything in an excel sheet. As the company made more and
-              more cheese, the excel sheet gets unmanageably large. The owner of
-              s28 does not plan to introduce a fully automated production line
-              in order to retain the artisanal cheese flavor to the maximum.
-              However, they do want a digitalization solution for their material
-              management process to monitor the ripening of each piece of cheese
-              and do order tracking.
+              We finished the implementation of the frontend using ReactJS and
+              the backend with Java Springboot. To enable this company in
+              possible future internationalization efforts, we have designed a
+              UI interface in multiple languages.
             </p>
-          </div>
-
-          <div className='px-6 pt-6'>
-            <p>1</p>
-          </div>
-
-          <div className='m-6 items-center text-center'>
-            <div className='flex justify-center items-center'>
-              <img
-                src={PicturePlaceHolder}
-                className='w-3/5 rounded-md shadow-xl'
-              />
-            </div>
-            <p className='text-xs text-gray-400'>Account Settings Page</p>
-          </div>
-
-          <div className='px-6'>
-            <p>p2</p>
           </div>
         </div>
       </div>
